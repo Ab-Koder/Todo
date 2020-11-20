50501e9ab0a8b27fc13f9faee28daea0343616a5
ab-koder <abdurrahmanyusufi478@gmail.com>
2020-11-20 18:54:48 +0530

    TODO COMMIT

---
+++ client/build/precache-manifest.b38a04ae82967f70be21548e826d0194.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32fd16f1c6be6fe5e0dfefb4196a3148",
+    "url": "/index.html"
+  },
+  {
+    "revision": "402f3f7ad7ac8a10ff24",
+    "url": "/static/css/main.01682200.chunk.css"
+  },
+  {
+    "revision": "38159b187a61a0ca4a9e",
+    "url": "/static/js/2.963c5385.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.963c5385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "402f3f7ad7ac8a10ff24",
+    "url": "/static/js/main.31a18a77.chunk.js"
+  },
+  {
+    "revision": "f80722c26d7fbe8d2608",
+    "url": "/static/js/runtime-main.9f7fbb3e.js"
+  }
+]);
